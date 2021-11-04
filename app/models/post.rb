77df13c2f37cf9d96b67c345cbe1c0769951f660
Patrick Wu <Ptrckwu@yahoo.com>
2021-11-04 10:36:01 -0400

    commit tree 84c8a4c22d4e253d77cf07424c63d2370c71f9a1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait 
+
+    def clickbait
+        errors.add("Title must be clickbaity") unless 
+        title.include?("Won't Believe") 
+    end
 end
